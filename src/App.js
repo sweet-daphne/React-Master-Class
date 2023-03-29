a787b217c9909cb5284c446d1c64b61883f59d26
suelrin <dancerelrin@gmail.com>
2023-03-30 07:20:32 +0900

    styled component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+const Circle = styled(Box)`
+  border-radius: 50px
+`
+
 function App() {
-  return <div style={{display:"flex"}}>
-      <div style={{backgroundColor:"teal", width:100,height:100}}></div>
-      <div style={{backgroundColor:"tomato", width:100,height:100}}></div>
-    </div>;
+  return (
+    <Father>
+      <Box bgColor="teal"/>
+      <Circle bgColor="tomato"/>
+    </Father>
+  );
 }
 
 export default App;
