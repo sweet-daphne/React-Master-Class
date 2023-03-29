a3c8a63aa6eb5a554992df02b3e715a63a3237c7
suelrin <dancerelrin@gmail.com>
2023-03-30 08:06:41 +0900

    animations and pseudo selectors

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Btn = styled.button`
-  color: white;
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
 `;
 
-const Input = styled.input.attrs({required:true})`
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation:${rotateAnimation} 2s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover{
+      font-size: 48px;
+    }
+    &:active{
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Btn>Login</Btn>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😂</span>
+      </Box>
+    </Wrapper>
   );
 }
 
