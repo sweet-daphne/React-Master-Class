6a66f5b56525d004dd58421035e1bb267800dae4
suelrin <dancerelrin@gmail.com>
2023-04-05 07:33:27 +0900

    ts state

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 interface ContainerProps{
@@ -16,14 +17,16 @@ const Container = styled.div<ContainerProps>`
 interface CircleProps{
   bgColor: string;
   borderColor?: string;
-  text?: string;
 }
 
-function Circle({bgColor, borderColor, text = "default text"}:CircleProps){
+function Circle({bgColor, borderColor}:CircleProps){
+  const [value, setValue] = useState<number|string>(0);
+  setValue(2);
+  setValue("Hello");
+  setValue(true);
+
   return (
-    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
-      {text}
-    </Container>
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />
   )
 }
 
